abf5e25d2e04eb411d5c4e4df88fcb64a4757dbe
Dario <darioescobar@gmail.com>
2026-01-08 15:42:46 -0300

    bkp

--- custom-app/frontend/app/lib/api.js
+++ custom-app/frontend/app/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api',
+    baseURL: (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001').replace(/\/$/, '') + '/api',
 });
 
 // Add a request interceptor to include the auth token
